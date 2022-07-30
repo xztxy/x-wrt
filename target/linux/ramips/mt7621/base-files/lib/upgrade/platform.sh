c7f5c2696f738301d32ff01b4d9983b831bd0cb6
Chen Minqiang <ptpt52@gmail.com>
2022-07-30 14:17:02 +0800

    ramips: init add mt7621_xwrt_nxc2005ex

--- target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/mt7621/base-files/lib/upgrade/platform.sh
@@ -194,6 +194,7 @@ platform_do_upgrade() {
 	xiaomi,mi-router-cr660x|\
 	z-router,zr-2660|\
 	z-router,zr-2662|\
+	xwrt,nxc2005ex|\
 	xwrt,nxc2009e-v100|\
 	xwrt,fm10-ax-nand|\
 	xwrt,wr1800k-ax-nand|\
