e94bdbe9a5295110a1441b5907fa59234a574480
Mikhail Zhilkin <csharper2005@gmail.com>
2024-11-17 16:30:31 +0000

    kernel: fix broken nmbm (Xiaomi AX3000t with Winbond spi-nand)
    Winbond W25N01KV spi-nand chip has 96 byte OOB size. At the same time
    stock u-boot use 64 byte spare area. As a result nmbm_attach fails on
    Xiaomi AX3000t with Winbond spi-nand:
    '''
    [    0.786783] NMBM configuration mismatch
    '''
    
    This commit makes nmbm_attach function return success if mtd oob size is
    greater than spare size and warn to the log:
    ...
    [    0.885464] Signature found at block 1023 [0x07fe0000]
    [    0.890620] NMBM spare size (64) is less than mtd oob size (96)
    [    0.896525] NMBM management region starts at block 960 [0x07800000]
    [    0.903799] First info table with writecount 1 found in block 960
    [    0.912267] Second info table with writecount 1 found in block 963
    [    0.918453] NMBM has been successfully attached
    ...
    
    Fixes: https://github.com/openwrt/openwrt/issues/16972
    Fixes: 81481c805aa56f6452d7e7577d42eb6b89a6869d ("generic: 6.6: add Winbond W25N01KV support")
    Signed-off-by: Mikhail Zhilkin <csharper2005@gmail.com>

--- target/linux/generic/files/drivers/mtd/nand/mtk_bmt_nmbm.c
+++ target/linux/generic/files/drivers/mtd/nand/mtk_bmt_nmbm.c
@@ -2200,10 +2200,14 @@ static int nmbm_attach(struct nmbm_instance *ni)
 	if (ni->signature.nand_size != bmtd.total_blks << bmtd.blk_shift ||
 	    ni->signature.block_size != bmtd.blk_size ||
 	    ni->signature.page_size != bmtd.pg_size ||
-	    ni->signature.spare_size != bmtd.mtd->oobsize) {
+	    ni->signature.spare_size > bmtd.mtd->oobsize) {
 		nlog_err(ni, "NMBM configuration mismatch\n");
 		return -EINVAL;
 	}
+
+	if (ni->signature.spare_size < bmtd.mtd->oobsize)
+		nlog_warn(ni, "NMBM spare size (%u) is less than mtd oob size (%u)\n",
+			 ni->signature.spare_size, bmtd.mtd->oobsize);
 
 	success = nmbm_load_existing(ni);
 	if (!success)
