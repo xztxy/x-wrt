c8b1e94b9af92b71f1d50a17c3939925a3452933
John Audia <therealgraysky@proton.me>
2025-08-16 09:19:55 -0400

    prereq-build.mk: optionally symlink pigz
    pigz[1] is a drop-in/fully functional replacement for gzip that exploits
    multiple processors and multiple cores when compressing data. If pigz is
    present on the host system, create the symlink for it under
    staging_dir/host/bin/ by calling SetupHostCommand.
    
    If pigz is not on the host, legacy gzip will be used.
    
    1. https://www.zlib.net/pigz
    
    Signed-off-by: John Audia <therealgraysky@proton.me>

--- include/image.mk
+++ include/image.mk
@@ -9,6 +9,12 @@ include $(INCLUDE_DIR)/kernel-defaults.mk
 include $(INCLUDE_DIR)/version.mk
 include $(INCLUDE_DIR)/image-commands.mk
 
+# Silently check for pigz and set GZIPCMD
+GZIPCMD := $(shell command -v pigz >/dev/null 2>&1 && echo pigz || echo gzip)
+ifndef CONFIG_TARGET_IMAGES_PIGZ
+  GZIPCMD := gzip
+endif
+
 ifndef IB
   ifdef CONFIG_TARGET_PER_DEVICE_ROOTFS
     TARGET_PER_DEVICE_ROOTFS := 1
@@ -91,7 +97,7 @@ SQUASHFSOPT += -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1'
 SQUASHFSOPT += $(if $(CONFIG_SELINUX),-xattrs,-no-xattrs)
 SQUASHFSOPT += -block-readers $(CONFIG_TARGET_SQUASHFS_BLOCK_READERS)
 SQUASHFSOPT += -small-readers $(CONFIG_TARGET_SQUASHFS_SMALL_READERS)
-SQUASHFSCOMP := gzip
+SQUASHFSCOMP := $(GZIPCMD)
 LZMA_XZ_OPTIONS := -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2
 ifeq ($(CONFIG_SQUASHFS_XZ),y)
   ifneq ($(filter arm x86 powerpc sparc,$(LINUX_KARCH)),)
@@ -183,7 +189,7 @@ endef
 ifdef CONFIG_TARGET_IMAGES_GZIP
   define Image/Gzip
 	rm -f $(1).gz
-	gzip -9n $(1)
+	$(GZIPCMD) -9n $(1)
   endef
 endif
 
@@ -366,13 +372,13 @@ ifdef CONFIG_TARGET_ROOTFS_TARGZ
   define Image/Build/targz
 	$(TAR) -cp --numeric-owner --owner=0 --group=0 --mode=a-s --sort=name \
 		$(if $(SOURCE_DATE_EPOCH),--mtime="@$(SOURCE_DATE_EPOCH)") \
-		-C $(TARGET_DIR)/ . | gzip -9n > $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz
+		-C $(TARGET_DIR)/ . | $(GZIPCMD) -9n > $(BIN_DIR)/$(IMG_PREFIX)$(if $(PROFILE_SANITIZED),-$(PROFILE_SANITIZED))-rootfs.tar.gz
   endef
 endif
 
 ifdef CONFIG_TARGET_ROOTFS_CPIOGZ
   define Image/Build/cpiogz
-	( cd $(TARGET_DIR); find . | $(STAGING_DIR_HOST)/bin/cpio -o -H newc -R 0:0 | gzip -9n >$(BIN_DIR)/$(IMG_ROOTFS).cpio.gz )
+	( cd $(TARGET_DIR); find . | $(STAGING_DIR_HOST)/bin/cpio -o -H newc -R 0:0 | $(GZIPCMD) -9n >$(BIN_DIR)/$(IMG_ROOTFS).cpio.gz )
   endef
 endif
 
@@ -771,7 +777,7 @@ define Device/Build/image
   .IGNORE: $(BIN_DIR)/$(call DEVICE_IMG_NAME,$(1),$(2))
 
   $(BIN_DIR)/$(call DEVICE_IMG_NAME,$(1),$(2)).gz: $(KDIR)/tmp/$(call DEVICE_IMG_NAME,$(1),$(2))
-	gzip -c -9n $$^ > $$@
+	$(GZIPCMD) -c -9n $$^ > $$@
 
   $(BIN_DIR)/$(call DEVICE_IMG_NAME,$(1),$(2)): $(KDIR)/tmp/$(call DEVICE_IMG_NAME,$(1),$(2))
 	cp $$^ $$@
